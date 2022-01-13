81ed164aad8d4d322499c637705f125293ea6188
CodeBottle <89330527+codebottlehun@users.noreply.github.com>
2022-01-13 15:38:25 +0900

    Add files via upload

---
+++ q4.py
@@ -0,0 +1,20 @@
+# Q4 Answer Template
+from functools import reduce
+
+def GCD(a, b):
+    '''
+    Euclid's Algorithm
+    Condition : The first parameter must be larger than second.
+    '''
+    while b:
+        a,  b = b, a % b
+    return a
+
+def solution(arr):
+    answer = int(reduce(lambda a, b : a * b / GCD(a, b), arr))
+    return answer
+
+arr = [None]*15
+arr = [2,6,8,14]
+answer = solution(arr)
+print(answer)
