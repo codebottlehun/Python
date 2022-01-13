858569d8f901b9f1c580a1561a69ee4aca27cb45
CodeBottle <89330527+codebottlehun@users.noreply.github.com>
2022-01-13 16:34:26 +0900

    Update q6.py

--- q6.py
+++ q6.py
@@ -1,14 +1,9 @@
 # Q6 Answer template
 
 def solution(arr):
-    idx = arr.index(min(arr))
-    li = []
-    for i, a in enumerate(arr):
-        if i == idx: continue
-        li.append(a) 
-
-    if not len(li): li.append(-1)
-    return li
+    arr.remove(min(arr))
+    if not len(arr): arr.append(-1)
+    return arr
 
 arr = [4, 3, 2, 1]
 #arr = [10]
